66ccf1ba7932f9c6509e77629044c1df8cf8a821
barrettMCW <mjbarrett@mcw.edu>
2022-10-03 13:26:18 -0500

    sorting environment WIP

--- postgres/XNAT.sql
+++ postgres/XNAT.sql
@@ -2,11 +2,11 @@
 
 \set xnat_user `echo $XNAT_DATASOURCE_USERNAME`
 \set xnat_user_pw `echo $XNAT_DATASOURCE_PASSWORD`
-\set xnat_db `echo $POSTGRES_DB`
+\set db `echo $POSTGRES_DB`
 
 drop role if exists :xnat_user;
 create role :"xnat_user" with login password :'xnat_user_pw';
 
 -- add missing grants for database
-ALTER DATABASE :"xnat_user" OWNER TO :"xnat_db";
+ALTER DATABASE :"xnat_user" OWNER TO :"db";
 
